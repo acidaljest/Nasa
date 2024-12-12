fb8174677a470a8d25d05d523e9918462d07d87a
Carolina Zárate Infante <122558293+acidaljest@users.noreply.github.com>
2024-12-12 15:40:22 -0300

    Add files via upload

---
+++ Space App/ejemplos/callback.js
@@ -0,0 +1,31 @@
+
+
+function uno (dos) {
+    console.log("uno")
+    setTimeout(dos,2000)
+}
+
+function dos () {
+    console.log("dos")
+
+}
+
+setTimeout(() => uno(dos),5000)
+
+
+
+
+
+const nombre = function () {
+    console.log("nombre")
+}
+
+nombre()
+
+
+const persona = () => console.log("Carolina")
+
+persona();
+
+() => console.log()
+
